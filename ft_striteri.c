71ec108d39738b6743f25092ac1da34f5836ba2f
Taichi Tashiro <tatashir@c1r23s4.42tokyo.jp>
2022-11-21 17:20:58 +0900

    libftcomp

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,25 +6,11 @@
 /*   By: tatashir <tatashir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 17:24:22 by tatashir          #+#    #+#             */
-/*   Updated: 2022/10/26 14:13:24 by tatashir         ###   ########.fr       */
+/*   Updated: 2022/11/21 17:17:34 by tatashir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// void	ft_striteri(char *s, void (*f)(unsigned int, char *))
-// {
-// 	unsigned int	i;
-
-// 	if (s == NULL || f == NULL)
-// 		return ;
-// 	i = 0;
-// 	while (s[i] != '\0')
-// 	{
-// 		(*f)(i, &s[i]);
-// 		i++;
-// 	}
-// }
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
